3a99fd68090889389055a1940cf272a446cbe2d5
jordanrob709 <jordanrob709@gmail.com>
2020-10-09 18:21:21 +0300

    complex state no.1

--- 1c/testapp/src/index.js
+++ 1c/testapp/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 
-const Display = ({counter}) => <div><p>{counter}</p></div>
+//const Display = ({counter}) => <div><p>{counter}</p></div>
   
 
-const Button = ({handleClick, text}) =><div><button onClick={handleClick}>{text}</button></div>
+//const Button = ({handleClick, text}) =><div><button onClick={handleClick}>{text}</button></div>
 
 
 
-
-
+// 1. SIMPLE STATE IMPLEMENTATION
+/*
 const App = () => {
 
   const [ counter, setCounter ] = useState(0)
@@ -34,6 +34,58 @@ const App = () => {
   )
 
 }
+*/
+
+
+
+// 2. COMPLEX STATE IMPLEMENTATION  first iteration
+const Display = ({counter}) => <div><p> {counter} </p></div>
+const Button = ({handleClick, text}) => <div><button onClick={handleClick} >{text}</button></div>
+
+
+const App = () => {
+  
+  //first 
+  /* 
+  const [ left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  */
+
+  //second
+  const [click, setClick] = useState({ left:0, right:0})
+
+
+  const leftClick = () => {
+    const newClick = {
+      left: click.left + 1,
+      right: click.right
+    }
+    setClick(newClick)
+
+  }
+  
+  const rightClick = () => {
+    const newClick = {
+      left: click.left,
+      right: click.right + 1
+    }
+    setClick(newClick)
+  }
+
+
+  return (
+    <div>
+      <Display counter={click.left} />
+      <Button handleClick={leftClick} text='left' />
+      <Button handleClick={rightClick} text='right' />
+      <Display counter={click.right} />
+    </div>
+  )
+
+}
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
