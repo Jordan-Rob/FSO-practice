58902801bc662e422f3c085c0117cf336fd8eb0e
jordanrob709 <jordanrob709@gmail.com>
2020-11-10 20:39:26 +0300

    practice on altering data in an api Part2 d)

--- 1c/testapp/src/index.js
+++ 1c/testapp/src/index.js
@@ -227,7 +227,15 @@ const App = () => {
   }
 
   const toggleImportanceOf = (id) => {
-    console.log('importance of ' + id + ' needs to be toggled')
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find( note => note.id === id)
+    const changedNote = { ...note, important: !note.important}
+
+    axios
+      .put( url, changedNote)
+      .then( response => {
+        setNotes(notes.map( note => note.id !== id? note:response.data))
+      })
   }
 
   const handleNoteChange = (event) =>{
