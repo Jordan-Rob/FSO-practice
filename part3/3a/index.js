c969b6c37b23c403a33a3d7ccb007923a1fc8b6a
jordanrob709 <jordanrob709@gmail.com>
2020-11-18 18:29:51 +0300

    Part3 a practice basic REST api

--- part3/3a/index.js
+++ part3/3a/index.js
@@ -4,6 +4,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
       id: 1,
@@ -53,6 +55,32 @@ app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
     notes = notes.filter(note => note.id !== id)
     response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+app.post('/api/notes', (request, response) => {
+    if (!body.content) {
+        return response.status(400).json({ 
+          error: 'content missing' 
+        })
+      }
+    
+      const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+      }
+    
+      notes = notes.concat(note)
+      response.json(note)
+
 })
 
 const PORT = 3001
