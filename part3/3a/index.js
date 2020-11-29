30844685d5beb3840f2b7bb9f971c4222bce347b
jordanrob709 <jordanrob709@gmail.com>
2020-11-29 03:18:10 +0300

    connected backend to database , modified request handlers to interact with db

--- part3/3a/index.js
+++ part3/3a/index.js
@@ -1,8 +1,11 @@
 //console.log('Hello World')
 
 //const http = require('http')
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note')
+
 const app = express()
 
 app.use(cors())
@@ -18,6 +21,7 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(requestLogger)
+
 
 let notes = [
     {
@@ -51,17 +55,16 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    //response.json(notes)
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if(note){
+    Note.findById(request.params.id).then( note => {
       response.json(note)
-    }else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -78,20 +81,23 @@ const generateId = () => {
   }
 
 app.post('/api/notes', (request, response) => {
-    if (!body.content) {
+    const body = request.body  
+
+    if (body.content === undefined ) {
         return response.status(400).json({ 
           error: 'content missing' 
         })
       }
     
-      const note = {
+      const note = new Note ({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId(),
-      }
-      notes = notes.concat(note)
-      response.json(note)
+      })
+      
+      note.save().then(result => {
+        response.json(result)
+      })
 
 })
 
@@ -101,7 +107,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 //app.listen(PORT)
 //console.log(`Server running on port ${PORT}`)
 
